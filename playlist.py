fe049758dc1a02d5f53f6c1babf95baac63f65ad
Shahvir <shahvirb@gmail.com>
2017-11-01 19:28:34 -0500

    created a module for the playlist parent class, based on the current version of liveplaylist

---
+++ playlist.py
@@ -0,0 +1,55 @@
+from setlist import *
+from gmusicapi import Mobileclient, utils
+import logging
+from apikey import *
+
+mlog = logging.getLogger(__name__)
+
+utils.utils.per_client_logging = False
+mlog.debug("gmusicapi logging monkey patch: {}".format(utils.utils.per_client_logging))
+
+
+class Playlist():
+    '''
+    Parent class for specific types of playlists, like:
+        Live Setlist Playlist
+            setlist based on the set of songs played at a live concert
+        Upcoming Concert Playlist
+            setlist generated from a list of bands that have upcoming concerts in an area
+    '''
+
+    def __init__(self, url, make=False):
+        self.setup_logging()
+        self.setlist = Setlist(url)
+        self.api = Mobileclient()
+        self.logged_in = self.api.login(EMAIL,
+            TOKEN,
+            Mobileclient.FROM_MAC_ADDRESS)
+        if self.logged_in:
+            self.search_for_songs()
+            if make:
+                self.make_playlist()
+
+    def setup_logging(self):
+        logger_name = '.'.join([__name__, __class__.__name__])
+        self.logger = logging.getLogger(logger_name)
+        logging.getLogger('gmusicapi.protocol.shared').setLevel(logging.INFO)
+        logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def create_playlist(self, name, description=''):
+        if hasattr(self, 'song_ids') and self.song_ids is not None:
+            self.id = self.api.create_playlist(name, description)
+            self.api.add_songs_to_playlist(self.id, self.songIDs)
+
+    def delete_playlist(self):
+        if hasattr(self, 'id') and self.id is not None:
+            self.info("Deleting playlist id: %s".format(self.id))
+            self.api.delete_playlist(self.id)
+        else:
+            self.info("Can't delete a playlist without its id")
