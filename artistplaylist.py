1207353a334d63c279eb84e79e1433923e89dfbc
Shahvir <shahvirb@gmail.com>
2017-11-04 16:23:32 -0500

    Instantiating ArtistPlaylist no longer creates a playlist

--- artistplaylist.py
+++ artistplaylist.py
@@ -5,24 +5,21 @@ class ArtistPlaylist(pl.Playlist):
     Playlist based on a list of artists
     '''
 
-    def __init__(self, artists):
+
+    def __init__(self):
         super().__init__()
-
         if self.logged_in:
-            print("GPMAA login successful")
-            song_ids = []
-            for a in artists:
-                song_ids.append(self.song_from_artist(a))
-            self.create_playlist(
-                song_ids,
-                name = "ArtistPlaylist",
-                description = ""
-                )
+            self.info("GPMAA login successful")
+            self.results = {}
 
-    def song_from_artist(self, artist):
-        self.results = self.api.search(artist)['song_hits']
-        self.results = [x['track'] for x in self.results]
-        return self.results[0]['storeId']
+    def songs_from_artist(self, artist):
+        self.results[artist] = [x['track'] for x in self.api.search(artist)['song_hits']]
+        return self.results[artist]
 
 if __name__ == '__main__':
-    ap = ArtistPlaylist(["King Gizzard"])
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    ap = ArtistPlaylist()
+    ap.songs_from_artist("King Gizzard")
+    print([song['storeId'] for song in ap.results["King Gizzard"]])
+    ap.api.logout()
