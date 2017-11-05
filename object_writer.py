b690fad63e157bac6d59282abc1dd6aa73a016a1
Shahvir <shahvirb@gmail.com>
2017-11-05 12:45:23 -0600

    cleanup whitespace

--- object_writer.py
+++ object_writer.py
@@ -1,5 +1,6 @@
 import pickle
 
+
 class ObjectWriter():
     def __init__(self, filename):
         self.filename = filename
@@ -14,6 +15,7 @@ class ObjectWriter():
         self.data = new_data
         with open(self.filename, 'wb') as file:
             pickle.dump(self.data, file)
+            
             
 if __name__ == '__main__':
     sw = ObjectWriter('object_writer.test')
